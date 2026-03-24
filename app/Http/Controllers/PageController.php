1b9470eb79026b8d1eb990cfe253c55f2d95f537
ZyadYhiaJisr <z.yhia@jisr.net>
2026-03-24 10:31:03 +0200

    add templates

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Inertia\Response;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+    public function index(): Response
+    {
+        return Inertia::render('Home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
-<?php
+\<?php
 
-use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
+    use App\Http\Controllers\ContactController;
+    use App\Http\Controllers\PageController;
+    use Illuminate\Support\Facades\Route;
+    use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+    // Route::inertia('/', 'welcome', [
+    //     'canRegister' => Features::enabled(Features::registration()),
+    // ])->name('home');
+    Route::get('/', [PageController::class, 'index'])->name('home');
+    Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
-});
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    });
 
-require __DIR__.'/settings.php';
+    require __DIR__ . '/settings.php';
