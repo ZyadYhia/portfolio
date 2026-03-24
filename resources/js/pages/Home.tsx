6459312d305ea52c4a3775db1cf9f83cbd1dbf10
ZyadYhiaJisr <z.yhia@jisr.net>
2026-03-24 11:44:51 +0200

    refactor: reorganize imports in Home and web routes for consistency

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -1,10 +1,10 @@
 import About from '@/components/main/About'
-import Contact from '@/components/main/Contact'
 import Hero from '@/components/main/Hero'
-import Projects from '@/components/main/Projects'
-import Skills from '@/components/main/skills'
+import Skills from '@/components/main/Skills'
 import Timeline from '@/components/main/Timeline'
 import MainLayout from '@/layouts/main-layout'
+import Contact from '@/components/main/Contact'
+import Projects from '@/components/main/Projects'
 
 export default function Home() {
     return (
